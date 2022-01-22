d45933af62bd735d24afc80c36d335c2acc0ed21
chuljin Hwang <chljin.hwang@gmail.com>
2022-01-22 14:33:10 +0900

    list-->Composer

--- SwiftUIMemo/Scene/ComposeScene.swift
+++ SwiftUIMemo/Scene/ComposeScene.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ComposeScene: View {
     @EnvironmentObject var store: MemoStore
     @State private var content: String = ""
-    
+    @Binding var showComposer : Bool
     
     var body: some View {
         NavigationView {
@@ -20,24 +20,26 @@ struct ComposeScene: View {
             }
             .frame(maxWidth: . infinity, maxHeight: .infinity) //가장 크게 설정
             .navigationBarTitle("새 메모", displayMode: .inline)
-            .navigationBarItems(leading: DissmissButton(), trailing: SaveButton())
+            .navigationBarItems(leading: DissmissButton(show: $showComposer), trailing: SaveButton(show: $showComposer))
         }
     }
 }
 
 fileprivate struct DissmissButton : View{
+    @Binding var show: Bool
     var body: some View{
         Button(action: {
-            
+            self.show = false
         }, label: {
             Text("취소")
         })
     }
 }
 fileprivate struct SaveButton: View{
+    @Binding var show: Bool
     var body: some View{
         Button(action: {
-            
+            self.show = false
         }, label: {
             Text("저장")
         })
@@ -46,7 +48,7 @@ fileprivate struct SaveButton: View{
 
 struct ComposeScene_Previews: PreviewProvider {
     static var previews: some View {
-        ComposeScene()
+        ComposeScene(showComposer: .constant(false)) //showComposer 값이 없으므로 Constant값 전달
             .environmentObject(MemoStore())
     }
 }
--- SwiftUIMemo/Scene/ContentView.swift
+++ SwiftUIMemo/Scene/ContentView.swift
@@ -10,14 +10,29 @@ import SwiftUI
 struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore //형식에 맞게 제공
     @EnvironmentObject var formatter: DateFormatter
+    @State var showComposer: Bool = false
+    
     var body: some View {
         NavigationView {
             List(store.list){ memo in
                 MemoCell(memo: memo)
             }
             .navigationBarTitle("내 메모")
+            .navigationBarItems(trailing: ModalButton(show: $showComposer)) //$붙이면 바인딩이 전달된다
+            .sheet(isPresented: $showComposer, content: {
+                ComposeScene(showComposer: self.$showComposer)
+            }) //$showCompser 값이 True면 ComposeScene()가 실형된다
         }
     }
+}
+fileprivate struct ModalButton: View{
+    @Binding var show: Bool //다른 곳의 속성을 바꾸기 위해 사용
+    var body: some View{
+        Button(action: {
+            self.show = true
+        }, label: { Image(systemName: "plus")
+    })
+  }
 }
 
 struct MemoListScene_Previews: PreviewProvider {
