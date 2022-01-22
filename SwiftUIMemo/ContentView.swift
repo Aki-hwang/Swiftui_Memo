40d2f82cf764e681dc313cfbb4bc4eb2338c845b
chuljin Hwang <chljin.hwang@gmail.com>
2022-01-22 11:53:14 +0900

    Initial commit

---
+++ SwiftUIMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by Chuljin Hwang on 2022/01/22.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
---
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMemoApp.swift
+//  SwiftUIMemo
+//
+//  Created by Chuljin Hwang on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MemoListScene()
+        }
+    }
+}
